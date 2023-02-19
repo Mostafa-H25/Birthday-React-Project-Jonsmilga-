a91cbf411340fd18079615f6c8fdc0307357886b
Mostafa-H25 <mostafa.hafez0@gmail.com>
2023-02-19 16:54:40 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data' 
+import React, {useState} from 'react'
 
 function App() {
+  const [people, setpeople] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <section className='container'>
+        <h3>Birthday Reminider</h3>
+        <h3>{people.length} Birthdays Today</h3>
+        {people.map((person) => {
+          return (
+            <article key={person.id} className='person'>
+              <img src={person.image} alt={person.name} />
+              <div>
+                <h4>{person.name}</h4>
+                <p>{person.age}</p>
+              </div>
+            </article>
+        )})}
+        <button onClick={() => setpeople([])}>Clear All</button>
+      </section>
+      
+    </main>
+  )
+};
+  
 
 export default App;
